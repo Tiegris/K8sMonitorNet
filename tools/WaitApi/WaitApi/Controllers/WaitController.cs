35a25512dcaf06fa2205a014ca77bd8ef083ef13
Tiegris <tigristibi@gmail.com>
2022-03-09 13:57:07 +0100

    Add WaitApi

---
+++ tools/WaitApi/WaitApi/Controllers/WaitController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WaitController : ControllerBase
+    {
+
+        private readonly ILogger<WaitController> _logger;
+
+        public WaitController(ILogger<WaitController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            return;
+        }
+
+        [HttpGet("{time}")]
+        public void Get([FromRoute] int time)
+        {
+            Thread.Sleep(time);
+            return;
+        }
+    }
+}
