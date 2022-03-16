2047aa146a2321ad5e1e32c6f51bfe09c795d118
Tiegris <tigristibi@gmail.com>
2022-03-16 19:14:46 +0100

    refactor

--- main/K8sMonitorCore/EndpointPinger/Endpoint.cs
+++ main/K8sMonitorCore/EndpointPinger/Endpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Pinger
 {
@@ -6,30 +7,19 @@ namespace Pinger
 
     public class Endpoint
     {
-
-        public event EndpointStatusChanged StatusChanged;
-        public StatusType Status { 
-            get => status; 
-            private set {
-                if (value != status) {
-                    StatusChanged?.Invoke(value);
-                }
-                status = value;
-            } 
-        }
+        public StatusType Status { get; set; }
         public int FailureThreshold { get; init; }
         public TimeSpan Timeout { get; init; }
         public TimeSpan Period { get; init; }
         public Uri Uri { get; init; }
 
         private int fails = 0;
-        private StatusType status;
 
         public void Fail() {
-            fails++;
-            if (fails >= FailureThreshold + 1) {
+            var temp = Interlocked.Increment(ref fails);
+            if (temp >= FailureThreshold + 1) {
                 Status = StatusType.Dead;
-                fails = FailureThreshold + 1;
+                Interlocked.Exchange(ref fails, FailureThreshold + 1);
             }
             else {
                 Status = StatusType.Dying;
@@ -37,10 +27,10 @@ namespace Pinger
         }
 
         public void Success() {
-            fails--;
+            Interlocked.Decrement(ref fails);
             if (fails <= 0) {
                 Status = StatusType.Healthy;
-                fails = 0;
+                Interlocked.Exchange(ref fails, 0);
             }
             else {
                 Status = StatusType.Recovering;
