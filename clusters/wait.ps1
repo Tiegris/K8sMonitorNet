2787172b59bf8198fd5bd68a4a809c7eb930ace0
Tiegris <tigristibi@gmail.com>
2022-03-30 19:59:24 +0200

    remote debugger

--- clusters/wait.ps1
+++ clusters/wait.ps1
@@ -1,6 +1,5 @@
 kubectl apply -f ./wait/ns.yaml
 kubectl config set-context --current --namespace="wait-ns"
 kubectl apply -f ./debugger.yaml
-kubectl apply -f ./bridge.yaml
 kubectl apply -f ./wait/deployments.yaml
 kubectl apply -f ./wait/services.yaml
