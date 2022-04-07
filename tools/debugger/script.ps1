3d51bb647dc314c939f1a29dc4b6feb906c7e3e8
Tiegris <tigristibi@gmail.com>
2022-04-07 16:25:02 +0200

    script runs with correct appsetting.json

--- tools/debugger/script.ps1
+++ tools/debugger/script.ps1
@@ -4,4 +4,4 @@ dotnet build $SOLUTION_FOLDER
 $POD = (k get pod -o name | Select-String debugger).Line.Split('/')[1]
 kubectl exec -it $POD -- rm -rf /sources
 kubectl cp ${SOLUTION_FOLDER} ${POD}:/sources/
-kubectl exec -it $POD -- dotnet /sources/K8sMonitorCore/bin/Debug/net6.0/K8sMonitorCore.dll
+kubectl exec -it $POD -- bash -c "cd /sources/K8sMonitorCore/bin/Debug/net6.0/ && dotnet K8sMonitorCore.dll"
