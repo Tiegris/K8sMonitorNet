5b9520a5ee66493ef7102ef0c5e1cc62c7dae1b6
Tiegris <tigristibi@gmail.com>
2022-04-12 19:51:58 +0200

    Razor polling

--- main/K8sMonitorCore/K8sMonitorCore/Pages/Index.cshtml.cs
+++ main/K8sMonitorCore/K8sMonitorCore/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using K8sMonitorCore.Aggregation.Dto.Simple;
+using K8sMonitorCore.Aggregation.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,13 +14,46 @@ namespace WebUi.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly AggregationService aggregation;
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        public IndexModel(ILogger<IndexModel> logger, AggregationService aggregation) {
             _logger = logger;
+            this.aggregation = aggregation;
         }
 
-        public void OnGet() {
+        public IEnumerable<SimpleStatusDto>? StatusList { get; set; }
 
+        public ICollection<SelectListItem>? GroupByOptions { get; set; }
+
+
+
+        public void OnGet() {
+            StatusList = aggregation.GetHealthGroupBySrv();
+
+            GroupByOptions = new SelectListItem[] {
+                new SelectListItem {
+                    Text = "Service",
+                    Value = "srv"
+                },
+                new SelectListItem {
+                    Text = "Namespace",
+                    Value = "ns"
+                }
+            };
+
+        }
+
+
+        [BindProperty]
+        public string? GroupById { get; set; }
+
+        public PartialViewResult OnPost() {
+            if (GroupById is "ns")
+                StatusList = aggregation.GetHealthGroupByNs();
+            else if (GroupById is "srv")
+                StatusList = aggregation.GetHealthGroupBySrv();
+
+            return Partial("_SimpleList", StatusList);
         }
     }
 }
