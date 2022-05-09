6dd74141c90d685b74b817d2ac01fe2064a3c28a
Tiegris <tigristibi@gmail.com>
2022-05-09 18:27:58 +0200

    debug script+

---
+++ tools/debugger/install_kubectl.sh
@@ -0,0 +1,4 @@
+apt update
+apt install -y curl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
--- tools/debugger/script.ps1
+++ tools/debugger/script.ps1
@@ -1,7 +1,8 @@
 F:
 $SOLUTION_FOLDER = "\UNI\MSc_Onlab_1\K8sMonitorNet\main\K8sMonitorCore\"
 dotnet build $SOLUTION_FOLDER
-$POD = (k get pod -o name | Select-String debugger).Line.Split('/')[1]
+$SELECTOR = "monitor"
+$POD = (k get pod -o name | Select-String $SELECTOR).Line.Split('/')[1]
 kubectl exec -it $POD -- rm -rf /sources
 kubectl cp ${SOLUTION_FOLDER} ${POD}:/sources/
 kubectl exec -it $POD -- bash -c "cd /sources/K8sMonitorCore/bin/Debug/net6.0/ && dotnet K8sMonitorCore.dll"
