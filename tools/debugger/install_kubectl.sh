4aa2fe0f797c74037aaf4579d2facea42955cb46
Tiegris <tigristibi@gmail.com>
2022-05-09 19:41:41 +0200

    helm clusterrole

--- tools/debugger/install_kubectl.sh
+++ tools/debugger/install_kubectl.sh
@@ -1,4 +1,6 @@
-apt update
-apt install -y curl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+apt update &&
+apt install -y curl &&
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
+apt update &&
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
+alias k=kubectl
