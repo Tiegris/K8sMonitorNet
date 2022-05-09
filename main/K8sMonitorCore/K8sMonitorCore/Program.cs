883e17c711f57e8f6d333a6ec56c776a9492afb2
Tiegris <tigristibi@gmail.com>
2022-05-09 16:12:39 +0200

    debug

--- main/K8sMonitorCore/K8sMonitorCore/Program.cs
+++ main/K8sMonitorCore/K8sMonitorCore/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading;
 
 namespace K8sMonitorCore;
 
@@ -8,7 +9,7 @@ public class Program
 {
     public static void Main(string[] args) {
         Console.WriteLine("Started Sleeping");
-        //Thread.Sleep(new TimeSpan(0, 0, 45));
+        Thread.Sleep(new TimeSpan(0, 0, 45));
         Console.WriteLine("Sleeping ended, starting app");
         CreateHostBuilder(args).Build().Run();
     }
--- tools/debugger/script.ps1
+++ tools/debugger/script.ps1
@@ -4,5 +4,5 @@ dotnet build $SOLUTION_FOLDER
 $POD = (k get pod -o name | Select-String debugger).Line.Split('/')[1]
 kubectl exec -it $POD -- rm -rf /sources
 kubectl cp ${SOLUTION_FOLDER} ${POD}:/sources/
-#kubectl exec -it $POD -- bash -c "cd /sources/K8sMonitorCore/bin/Debug/net6.0/ && dotnet K8sMonitorCore.dll"
-kubectl exec -it $POD -- dotnet run --project /sources/K8sMonitorCore/K8sMonitorCore.csproj
+kubectl exec -it $POD -- bash -c "cd /sources/K8sMonitorCore/bin/Debug/net6.0/ && dotnet K8sMonitorCore.dll"
+#kubectl exec -it $POD -- dotnet run --project /sources/K8sMonitorCore/K8sMonitorCore.csproj
